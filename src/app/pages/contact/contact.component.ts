f24a77273c695bed8a2db3a8d0794c9f1dd9230d
Bryan <Bryan@BRYAN>
2024-10-06 21:57:05 +0200

    Done

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,19 +1,5 @@
 import { Component } from '@angular/core';
 
-import {
-  faMapMarkerAlt,
-  faPhone,
-  faEnvelope, faArrowLeftRotate, faShare
-} from '@fortawesome/free-solid-svg-icons';
-import {
-  faFacebookF,
-  faTwitter,
-  faInstagram,
-  faLinkedinIn, faYoutube
-} from '@fortawesome/free-brands-svg-icons';
-
-import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
-
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
